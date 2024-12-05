0bf4acdfe09a2ef72534bb030defaaf6db6d42c8
Lulu Khaira <lulukxhaira@gmail.com~>
2024-12-05 14:02:27 +0700

    Portofolio-SMK-NEGERI-7-BATAM

---
+++ src/app/shared/global-constant.spec.ts
@@ -0,0 +1,7 @@
+import { Globalconstant } from './global-constant';
+
+describe('GlobalConstant', () => {
+  it('should create an instance', () => {
+    expect(new Globalconstant()).toBeTruthy();
+  });
+});
